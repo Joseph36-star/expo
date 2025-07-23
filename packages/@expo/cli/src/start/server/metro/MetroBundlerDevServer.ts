e3b99273af7f869446aee93eab43623df2d6da8f
Krystof Woldrich <krystofwoldrich@gmail.com>
2025-07-23 22:17:41 +0200

    [expo/cli] Remove duplicates and irrelevant information from transform error messages

--- packages/@expo/cli/src/start/server/metro/MetroBundlerDevServer.ts
+++ packages/@expo/cli/src/start/server/metro/MetroBundlerDevServer.ts
@@ -35,6 +35,7 @@ import { ExpoRouterServerManifestV1, fetchManifest } from './fetchRouterManifest
 import { instantiateMetroAsync } from './instantiateMetro';
 import {
   attachImportStackToRootMessage,
+  dropStackIfContainsCodeFrame,
   getErrorOverlayHtmlAsync,
   IS_METRO_BUNDLE_ERROR_SYMBOL,
 } from './metroErrorInterface';
@@ -1573,6 +1574,7 @@ export class MetroBundlerDevServer extends BundlerDevServer {
         }
       } catch (error) {
         attachImportStackToRootMessage(error);
+        dropStackIfContainsCodeFrame(error);
         throw error;
       }
 
